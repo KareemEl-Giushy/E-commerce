a92628688613bcd3fd43f79afd0e1bf7453bed71
= <=>
2019-05-12 11:42:59 +0200

    making a navbar but the idea isnt too great

--- includes/templates/navbar.php
+++ includes/templates/navbar.php
@@ -12,16 +12,20 @@
       </button>
 
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <form class="form-inline my-2 my-lg-0">
+          <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+          <button class="btn btn-outline-primary my-2 my-sm-0" type="submit"><i class='fa fa-search'></i></button>
+        </form>
         <ul class="navbar-nav mr-auto">
 <?php
-          foreach (getgitems('categories', 'WHERE visibility = 1 AND parent = 0', 'cid') as $cate) {
+          /*foreach (getgitems('categories', 'WHERE visibility = 1 AND parent = 0', 'cid') as $cate) {
             echo "<li class='nav-item'>";
             echo "<a class='nav-link' href='category.php?pageid=" . $cate[0] . "&pagename=" . str_replace(" ", "-",$cate[1]) . "'>" . $cate[1] . "</a>";
             echo "</li>";
-          } ?>
+          } */?>
           <?php if (isset($_SESSION['fnorname']) && isset($_SESSION['norid'])): ?>
             <li class="nav-item dropdown">
-              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              <a class="nav-link dropdown-toggle l-capital" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                 <?php echo $_SESSION['fnorname']; ?>
               </a>
               <div class="dropdown-menu" aria-labelledby="navbarDropdown">
@@ -37,14 +41,19 @@
               </div>
             </li>
           <?php else: ?>
-            <div class="signs">
-              <li class="nav-item float-left">
-                <a class="nav-link" href="login.php?act=signup-form" style="border: 3px solid #cdcdcdcd;">Sign UP</a>
-              </li>
-              <li class="nav-item float-left">
-                <a class="nav-link" href="login.php">login</a>
-              </li>
-            </div>
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                Your Account
+              </a>
+              <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                <div class="downdroping" href="#">New Customer ?</div>
+                <div class="signin-btn text-center m-3">
+                  <a href="login.php?act=signup-form" class="">Sign In</a>
+                </div>
+                <div class="dropdown-divider"></div>
+                <div class="downdroping text-center" href="#">Already Here <a href="login.php">Sign in</a></div>
+              </div>
+            </li>
           <?php endif; ?>
         </ul>
       </div>
